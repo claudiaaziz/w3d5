a9309f573d608abed8766eedefaecc9da24655ad
claudiaaziz <claudiahannahh@gmail.com>
2023-09-08 14:42:58 -0400

    did parent= & initialize method

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
-  
+    attr_reader :value, :children, :parent
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end 
+
+    def parent=(node)
+        self.parent.children.delete(self) if self.parent != nil
+        @parent = node
+        @parent.children << self if @parent != nil  
+    end
+
+    d
 end
